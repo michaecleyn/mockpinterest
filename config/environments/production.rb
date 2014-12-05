e100bb55766af9ba51774ff9921e5f28baf8504e
Michael Cleyn <michael.cleyn@shopify.com>
2014-12-05 11:22:46 -0500

    add } on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
 
   #Required for Heroku
   #Set to actual host name
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
